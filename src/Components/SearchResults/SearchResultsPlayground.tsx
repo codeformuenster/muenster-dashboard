08597b2c5559b2f48dcbb6e39f61df423937b0d6
Yannic Schencking <y.schencking@gruenspar.com>
2017-11-11 11:31:41 +0100

    Complete playground search results

--- src/Components/SearchResults/SearchResultsPlayground.tsx
+++ src/Components/SearchResults/SearchResultsPlayground.tsx
@@ -25,20 +25,12 @@ class SearchResultsPlayground extends SearchResults {
         <div className="media-content">
           <div className="content">
             <span className="title">
-              <span>{result.name} &nbsp; </span>
+              <span>{this.displayName(result.name)} &nbsp; </span>
               <span className="tag is-dark">Spielplatz</span> &nbsp;
-              <span className="tag is-success">
-                {this.displayArea(result.properties.Bereich)}
-      {/*}
-      Ball 2
-      Bereich A
-      Groesse 2069qm
-      Skater
-      Streetball */}
-
-              </span> &nbsp;
-              {this.displayPitch(result.properties.Ball) && <span className="icon"> <i className="mdi mdi-soccer"></i> </span>}
-              <span> <img className="tag-image" src="/media/skateboard.png"/> </span>
+              {result.properties.Groesse && <span className="tag is-dark"> {result.properties.Groesse} m<sup>2</sup></span>} &nbsp;
+              {this.displayPitch(result.properties.Ball, result.properties.Streetball) && <span className="icon"> <i className="mdi mdi-soccer"></i> </span>} &nbsp;
+              {this.displaySkater(result.properties.Skater) && <span> <img className="tag-image" src="/media/skateboard.png"/> </span>} &nbsp;
+              {result.properties.Bereich && <span className="tag is-success"> {this.displayArea(result.properties.Bereich)} </span>} &nbsp;
             </span>
             <div className="is-clearfix">
               {result.description}
@@ -60,7 +52,14 @@ class SearchResultsPlayground extends SearchResults {
     );
   }
 
-  protected displayArea(area: string): string {
+  private displayName(name: string): string{
+    let result = '';
+    result = name.slice(3);
+
+    return result.toLowerCase().replace(/\b\w/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+
+  private displayArea(area: string): string {
     let result = '';
 
     switch (area) {
@@ -81,63 +80,13 @@ class SearchResultsPlayground extends SearchResults {
     return result;
   }
 
-  protected displayPitch(pitch: number): boolean {
-    return pitch == 1 || pitch == 2
+  private displayPitch(ball: number, streetBall: string): boolean {
+    return ball === 1 || ball === 2 || streetBall === 'ja'
   }
-  //private distancePrettifier(dist: number): string {
 
-    //return '' + Math.round(dist) + 'm';
-  //}
-
-  /**
-   * Onclick handler for search result row, updates selectedId in searchParams
-   */
-  //private toggleSelection = (event: any, id: number) => {
-
-    //let searchParams = this.props.searchParams;
-    //if (searchParams.selectedId === id) {
-      //searchParams.selectedId = 0;
-    //} else {
-      //searchParams.selectedId = id;
-    //}
-    //this.props.updateHandler(searchParams);
-  //}
-
-  /**
-   * get the feature icons html
-   */
-  //private renderFeatures(features: string[]) {
-    //let iconRows = [];
-
-    //for (let feature of features) {
-      //iconRows.push(this.getFeatureIcon(feature));
-    //}
-    //return iconRows;
-  //}
-
-  /**
-   * Get icon html for the feature named $feature
-   */
-  //private getFeatureIcon(feature: string) {
-
-    //const iconList = {
-      //wifi: ['wifi', 'W-Lan'],
-      //cash: ['money', 'Barzahlung'],
-      //debit_card: ['credit-card', 'EC-Karte'],
-      //credit_card: ['cc-visa', 'Kreditkarte'],
-      //special_card: ['warning', 'Bezahlen nur mit spezieller Karte. Automat am Eingang.']
-    //};
-    //const iconSettings = iconList[feature] ? iconList[feature] : null;
-    //if (iconSettings) {
-      //return (
-        //<span className="icon" data-balloon={iconSettings[1]} data-balloon-pos="up">
-          //<i className={'fa fa-' + iconSettings[0]}></i>
-        //</span>
-      //);
-    //}
-    //console.log('Unrecognized feature', feature);
-    //return null;
-  //}
+  private displaySkater(skater: string): boolean {
+    return skater === 'ja';
+  }
 }
 
 export default SearchResultsPlayground;
