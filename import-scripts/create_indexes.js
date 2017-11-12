c1fe268fa626cb996c4c69918398020bdc4c7f34
ubergesundheit <ubergesundheit@googlemail.com>
2017-11-12 13:35:33 +0100

    add script to create indexes

---
+++ import-scripts/create_indexes.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const request = require('request-promise-native');
+
+const baseUrl = '';
+
+const placesIndex = {
+  "mappings": {
+    "place": {
+      "properties": {
+        "date_start": {
+          "type": "date"
+        },
+        "date_end": {
+          "type": "date"
+        },
+        "address": {
+          "properties": {
+            "geo": {
+              "type": "geo_point"
+            },
+            "geometry": {
+              "type": "geo_shape"
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const districtIndex = {
+  "mappings": {
+    "stadtteil": {
+      "properties": {
+        "geometry": { "type": "geo_shape" },
+        "center": { "type": "geo_shape" }
+      }
+    }
+  }
+};
+
+const indexes = {
+  "places": placesIndex,
+  "stadtteile": districtIndex
+};
+
+for (const [indexName, mapping] of Object.entries(indexes)) {
+  request.put({
+      url: `${baseUrl}/${indexName}`,
+      json: true,
+      body: mapping
+    })
+    .then(function (response) {
+      console.log(`Successfully created index ${indexName}`);
+      console.log(response);
+    })
+    .catch(function (err) {
+      console.log(`Error creating index ${indexName}`);
+      console.log(err.message);
+    });
+}
+
