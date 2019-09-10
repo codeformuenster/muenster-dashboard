48497c59cb558c0f0140bae20de1d4d780a7ebff
Thomas Werner <tom@input23.de>
2019-09-10 20:08:57 +0200

    Change the elastic search default hostname

--- src/Services/SearchService.js
+++ src/Services/SearchService.js
@@ -1,5 +1,5 @@
 import * as Elasticsearch from 'elasticsearch'
-import { getBaseUrl, switchElasticSearchHost, placesIndex } from '../Constants/Elasticsearch'
+import { getBaseUrl, placesIndex } from '../Constants/Elasticsearch'
 
 const client = new Elasticsearch.Client({
   host: getBaseUrl(),
@@ -186,8 +186,7 @@ export class SearchService {
     client
       .search(searchQuery, ((error, body) => {
         if (error) {
-          console.trace('error', error.message)
-          switchElasticSearchHost()
+          console.trace('error #239 -- ', error.message)
         }
         const locations = []
         if (body && body.hits) {
