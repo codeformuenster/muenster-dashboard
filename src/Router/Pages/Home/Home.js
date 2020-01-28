f2e2db9a8d2638d57bf03c18e1c4806fa0ca5251
Uria Rogel <uria_rogel@hotmail.com>
2020-01-28 20:29:45 +0100

    removed red background, filling up page

--- src/Router/Pages/Home/Home.js
+++ src/Router/Pages/Home/Home.js
@@ -12,9 +12,8 @@ const Layout = styled.div`
   position: relative;
   width: 100vw;
   max-width: 100vw;
-  height: 100vh;
-  max-height: 100vh;
-  background: red;
+  height: 100%;
+  max-height: 100%;
 `
 
 export class Home extends Component {
